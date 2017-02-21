901e16ef7bdb1fccb27b2032b1a3414c6ae1a476
Henry Chong <huashiuanchong@gmail.com>
2017-02-21 09:13:14 +0800

    Initial Commit

---
+++ firstTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  firstTest
+//
+//  Created by Henry Chong on 2/21/17.
+//  Copyright © 2017 Henry Chong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
